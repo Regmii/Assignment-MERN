c997ee3640071cf57675d7cdc561e2b818f87a9c
unknown <aadarsha.regmi11@gmail.com>
2024-06-04 11:28:54 +0545

    test1

---
+++ javascript/array/level1.js
@@ -0,0 +1,19 @@
+const arr = [3,5,1,2,9,12]
+//write your code here
+arr.filter((item,id)=>{
+  if(item%3===0){
+    return item
+  }
+})
+console.log(item,id)
+
+//expected output---> [3,9,12]
+// hint: %3 === 0
+// filter
+//syntax:
+// arr.filter((item,id)=>{
+//     //
+// })
+
+
+//----------------
