2a8b45a687bae6547172d387e0b39f690f236be3
unknown <aadarsha.regmi11@gmail.com>
2024-06-04 17:49:44 +0545

    test

---
+++ javascript/array/level2.js
@@ -0,0 +1,13 @@
+const arr = ['kaylin', 'ram', 'gopal', 'ankit']
+
+const userDetails = {
+    'kaylin': 'khanal',
+    'alisha': 'rauniyar',
+    'ayush': 'rai',
+    'gopal': 'tharu'
+}
+arr.map((item)=>{
+  if(userDetails[item]){
+    return item +' '+ userDetails[item]     
+  }  
+}).filter(item=>item)
